ddd80b085bb7884fc66e8a81212d266fc3dc2942
Mashi Khan <mashikhanwork@gmail.com>
2026-08-05 10:37:10 +0530

    feat: enhance sidebar functionality with collapsible elements and improved layout

--- frontend/src/features/auth/components/Sidebar.jsx
+++ frontend/src/features/auth/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {  FiSidebar } from "react-icons/fi";
 import { GiArtificialHive } from "react-icons/gi";
-import { motion } from "motion/react";
+import { motion, transform } from "motion/react";
 import { FiFileText } from "react-icons/fi";
 import { FiMap } from "react-icons/fi";
 import { FiStar } from "react-icons/fi";
@@ -37,11 +37,14 @@ const Sidebar = ({ collapsed, setCollapsed }) => {
           <div className="w-7 h-7 rounded-full bg-[#0A0A0A] flex items-center justify-center">
             <GiArtificialHive size={15} />
           </div>
-          <span className="font-sans font-extrabold text-base tracking-tight text-black">
-            PathPilot
-          </span>
+          {!collapsed && (
+            <span className="font-extrabold text-base tracking-tight text-black">
+              PathPilot
+            </span>
+          )}
         </div>
-        <div className="text-black px-2  font-bold cursor-pointer">
+        <div  onClick={() => setCollapsed(!collapsed)}
+        className="text-black px-2 h-8 w-8 flex items-center bg-white/75 rounded-full hover:bg-gray-200 hover:scale-105 font-bold cursor-pointer">
           <FiSidebar />
         </div>
       </div>
@@ -55,7 +58,7 @@ const Sidebar = ({ collapsed, setCollapsed }) => {
       >
         <button className="flex items-center justify-between gap-4 px-4  py-2 bg-black rounded-xl cursor-pointer">
           <span className="font-bold text-white "><FiPlus /></span>
-          <span className="text-sm">Create Interview</span>
+          {!collapsed  && <span className="text-sm">Create Interview</span>}
         </button>
       </motion.div>
 
@@ -67,11 +70,11 @@ const Sidebar = ({ collapsed, setCollapsed }) => {
             {/* Nav-Label  */}
             {NAV_ITEMS.map((item, idx) => (
               <div className="py-2">
-                <motion.button className="flex items-center gap-2 px-4 cursor-pointer">
-                  <span className="text-[12px] text-black/50">{item.icon}</span>
-                  <span className="text-[10px] sm:text-xs text-black/75">
+                <motion.button className="flex items-center gap-2 px-4 py-2 cursor-pointer hover:bg-gray-200">
+                  <span className="text-[12px] text-black/50 hover:font-bold">{item.icon}</span>
+                  {!collapsed && <span className="text-[10px] sm:text-xs text-black/75 hover:text-black/75">
                     {item.label}
-                  </span>
+                  </span>}
                 </motion.button>
               </div>
             ))}
@@ -79,48 +82,86 @@ const Sidebar = ({ collapsed, setCollapsed }) => {
         </div>
 
         {/* footer  */}
-        <div className=" flex flex-col px-4 gap-2 ">
-          {/* Interview coins */}
-          <div className="bg-[#0A0A0A] flex  justify-between items-center px-4 py-2 rounded-md gap-4 cursor-pointer">
-            <div className="flex items-center gap-2">
-              <span className="text-yellow-400"><WiStars size={24}/></span>
-           <div className="flex flex-col">
-             <span className="text-white text-xs">Interview Coins</span>
-            <span className="text-white text-xs">3500</span>
-           </div>
-            <div>
-            </div>
-            </div>
-            <div className="h-4 w-4 bg-gray-600 flex items-center justify-center rounded-full shadow-[0_0_0_2px_#0A0A0A] ">
-                <FiPlus className="text-white" size={14} />
-            </div>
-          </div>
-          {/* account */}
-          <div className="bg-gray-200 border flex justify-between items-center px-4 py-2 my-2 rounded-md gap-4 cursor-pointer">
-            <div className="flex gap-2 items-center ">
-             <div className="w-8 h-8 rounded-full bg-[#0A0A0A] flex items-center justify-center border">
-  <span className="text-[10px] font-semibold text-white">MM</span>
-</div>
-             {/* Name  */}
-             <div className="cursor-pointer m">
-              <span className="text-xs text-black font-sans ">MOHD MASIHULLA</span>
-              <p className="text-[10px] text-gray-400">mashikhanwork@gmail.com</p>
-             </div>
-            </div>
-            <div>
-              <FaRightFromBracket className="text-black " size={12} />
-            </div>
-          </div>
+      <div className="flex flex-col px-2 gap-2">
+
+  {/* Interview Coins */}
+  <div 
+    className={`bg-[#0A0A0A] flex items-center rounded-md cursor-pointer group
+    ${collapsed ? "justify-center p-3" : "justify-between px-4 py-2"}`}
+  >
+
+    <div className={`flex items-center ${collapsed ? "" : "gap-2"}`}>
+      <span className="text-yellow-400">
+        <WiStars size={24}/>
+      </span>
+
+      {!collapsed && (
+        <div className="flex flex-col">
+          <span className="text-white text-xs">
+            Interview Coins
+          </span>
+          <span className="text-white text-xs">
+            3500
+          </span>
         </div>
+      )}
+    </div>
+
+
+    {!collapsed && (
+      <div className="h-4 w-4 bg-gray-600 flex items-center justify-center rounded-full">
+        <FiPlus className="text-white" size={14}/>
+      </div>
+    )}
+
+  </div>
+
+
+  {/* Account */}
+  <div 
+    className={`bg-gray-200 border rounded-md cursor-pointer flex items-center
+    ${collapsed ? "justify-center p-2" : "justify-between px-4 py-2"}`}
+  >
+
+    <div className="flex items-center gap-2">
+
+      <div className="w-8 h-8 rounded-full bg-black flex items-center justify-center">
+        <span className="text-[10px] font-semibold text-white">
+          MM
+        </span>
+      </div>
+
+
+      {!collapsed && (
+        <div>
+          <span className="text-xs text-black">
+            MOHD MASIHULLA
+          </span>
+
+          <p className="text-[10px] text-gray-400">
+            mashikhanwork@gmail.com
+          </p>
+        </div>
+      )}
+
+    </div>
+
+
+    {!collapsed && (
+      <FaRightFromBracket size={12}/>
+    )}
+
+  </div>
+
+</div>
       </div>
     </div>
   );
 
   return (
     <motion.aside
-      initial={{ opacity: 0, x: -15 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.5 }}
+      animate = {{ width: collapsed ? 72 : 250 }}
+      transition={{ duration: 0.5 ,ease: "easeInOut" }}
       className="fixed top-0 left-0 h-screen w-72 bg-white/75 shadow-2xl"
     >
       {innerPart}
