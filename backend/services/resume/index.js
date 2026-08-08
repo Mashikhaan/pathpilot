8a2279bf538bb088f7fc6689c5a35a373a93c55e
Mashi Khan <mashikhanwork@gmail.com>
2026-08-08 15:09:50 +0530

    feat(resume): add resumeRouter for handling upload and retrieval of resumes

--- backend/services/resume/index.js
+++ backend/services/resume/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from "./config/db.js";
-
+import resumeRouter from "./routes/resume.route.js";
 
 
 
@@ -11,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 6002
 
 app.use(express.json());
+app.use("/", resumeRouter);
+
 
 app.get("/", (req, res)=>{
     res.send("hello from resume service");
---
+++ backend/services/resume/routes/resume.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import upload from "../middleware/multer.js";
+import { getResumeController, uploadResumeController } from "../controllers/resume.controller.js";
+
+
+const resumeRouter = express.Router();
+
+
+//upload resume route
+resumeRouter.post("/upload",upload, uploadResumeController);
+
+//get resume route
+resumeRouter.get("/get-resume", getResumeController );
+
+
+export default resumeRouter;
